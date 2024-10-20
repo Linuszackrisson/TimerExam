b59cd2b3eba3440653b6b19797a036c1a18df10d
Linus Zackrisson <linus.gz@hotmail.com>
2024-10-21 00:53:17 +0200

    no animations

--- src/components/End.jsx
+++ src/components/End.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Ändrad till useNavigate
 import alarmIcon from '../assets/alarmicon.svg'; // Importera SVG-filen
-import cancelTimer from '../components/TimerPage.jsx';
 
 function End() {
+  const navigate = useNavigate(); // Skapa en instans av useNavigate
+
+  const handleButtonClick = () => {
+    navigate('/set-timer'); // Navigera till TimerPage med useNavigate
+  };
+
   return (
     <div className="end-page">
+      <div className="end-mid">
       <img src={alarmIcon} alt="Alarm Icon" className="alarm-icon" /> {/* Visa SVG-ikonen */}
       <h1 className='times-up'>Times up!</h1>
-      <button className='newtime-button' onClick={cancelTimer}>SET NEW TIMER</button>
+      </div>
+      <button className='newtime-button' onClick={handleButtonClick}>SET NEW TIMER</button> {/* Ändrad onClick-funktion */}
 
     </div>
   );
