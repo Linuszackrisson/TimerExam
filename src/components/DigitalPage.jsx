01a47253603458a97e26bdc3e95ae95643d7d0a7
Linus Zackrisson <linus.gz@hotmail.com>
2024-10-21 12:39:59 +0200

    added animatons

--- src/components/DigitalPage.jsx
+++ src/components/DigitalPage.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 function DigitalPage({ secondsRemaining }) {
-  // Beräkna minuter och sekunder från sekunder kvar
   const minutes = Math.floor(secondsRemaining / 60);
   const seconds = secondsRemaining % 60;
 
   return (
     <div className='digital-page'>
-      <h2 className='digital-clock'>{`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`}</h2>
+      <motion.h2 
+        className='digital-clock' 
+        animate={{ y: [0, -10, 0] }} 
+        transition={{ duration: 2, repeat: Infinity }} 
+      > {/* Målet här var att försöka få en svävande effekt så vi animerar y-axel och det tar två sekunder att nå målet o tillbaks,
+      även denna loopar.*/}
+        {`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`}
+      </motion.h2>
     </div>
   );
 }
